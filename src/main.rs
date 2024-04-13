7cc58fb74a73604add5224ff3adbce107ceff7ac
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2024-04-13 12:26:54 +0200

    retrieve html response

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
+async fn retrieve_html() -> String {
+    let response = reqwest::get("https://news.ycombinator.com").await.unwrap().text().await.unwrap();
+    return response;
+}
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -10,6 +13,11 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+#[get("/scraper")]
+async fn scraper() -> impl Responder {
+    let response = retrieve_html().await;
+    HttpResponse::Ok().body(response)
+}
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
@@ -19,6 +27,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
            .service(hello)
            .service(echo)
+           .service(scraper)
            .route("/hey",web::get().to(manual_hello))
     })
    .bind("127.0.0.1:8080")?
