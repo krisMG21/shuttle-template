1d6cb2fdddb0f9f9c671752a31527ac37383f823
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2024-04-13 13:11:07 +0200

    remove unneeded functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use ::scraper::{Html, Selector};
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 async fn retrieve_html() -> String {
     let response = reqwest::get("https://news.ycombinator.com")
         .await
@@ -23,35 +23,16 @@ fn parse_html(response: &String) -> String {
     result
 }
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
 #[get("/scraper")]
 async fn scraper() -> impl Responder {
     let response = retrieve_html().await;
     let parsed_response = parse_html(&response);
     HttpResponse::Ok().body(parsed_response)
 }
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .service(scraper)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(scraper))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
