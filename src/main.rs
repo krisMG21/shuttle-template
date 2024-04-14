fc695c2b20cfb8c39b04cda4af71594343bef689
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2024-04-14 12:14:07 +0200

    refactor: create a function for element selection

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,8 @@ async fn retrieve_html() -> String {
         .unwrap();
     return response;
 }
-fn extract_titles(response: &String) -> Vec<String> {
-    let doc_body = Html::parse_document(&response);
-    let title = Selector::parse(".titleline").unwrap();
+async fn extract_titles() -> Vec<String> {
+    let (doc_body, title) = select_el(".titleline").await;
 
     let mut titles = Vec::new(); // declare empty vector to hold titles
     for title in doc_body.select(&title) {
@@ -23,10 +22,16 @@ fn extract_titles(response: &String) -> Vec<String> {
     titles
 }
 
+async fn select_el(selector: &str) -> (Html, Selector) {
+    let response = retrieve_html().await;
+    let document = Html::parse_document(&response);
+    let title = Selector::parse(selector).unwrap();
+    (document, title)
+}
+
 #[get("/")]
 async fn scraper() -> impl Responder {
-    let response = retrieve_html().await;
-    let parsed_response = extract_titles(&response);
+    let parsed_response = extract_titles().await;
     HttpResponse::Ok().json(parsed_response)
 }
 #[actix_web::main]
