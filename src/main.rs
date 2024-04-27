5e9dde50bc01207f5afbb547017b2612f5ff3a4a
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2024-04-27 19:18:55 +0300

    new endpoint for returning country details

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::BTreeMap;
 
 use ::scraper::{Html, Selector};
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use serde::Serialize;
 #[derive(Serialize)]
 struct Country {
@@ -42,6 +42,16 @@ async fn select_el(selector: &str) -> Selector {
     country_name_selector
 }
 
+#[get("/country/{name}")]
+async fn get_country(name: web::Path<String>) -> impl Responder {
+    let parsed_response = extract_countries().await;
+    let (country, capital) = parsed_response.get_key_value(&name.to_string()).unwrap();
+    HttpResponse::Ok().json(web::Json(Country {
+        name: country.to_string(),
+        capital: capital.to_string(),
+    }))
+}
+
 #[get("/")]
 async fn scraper() -> impl Responder {
     let parsed_response = extract_countries().await;
@@ -56,7 +66,7 @@ async fn scraper() -> impl Responder {
 }
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(scraper))
+    HttpServer::new(|| App::new().service(scraper).service(get_country))
         .bind("127.0.0.1:8080")?
         .workers(4) // turn this into a multi-thread server
         .run()
