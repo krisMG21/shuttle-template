ea6c9bc323966c1462dff3fcabe2a507dbe65679
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2024-06-16 16:24:02 +0300

    add env variable

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,16 @@ use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
 use countries::{extract, Country};
 
+use dotenv::dotenv;
+use lazy_static::lazy_static;
+use std::env;
+lazy_static! {
+    static ref PORT: String = {
+        dotenv().ok(); // Load .env file if using dotenv
+        env::var("PORT").expect("PORT must be set")
+    };
+}
+
 #[get("/country/{name}")]
 async fn get_country(name: web::Path<String>) -> impl Responder {
     let parsed_response = extract().await;
@@ -30,7 +40,7 @@ async fn scraper() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(scraper).service(get_country))
-        .bind("127.0.0.1:8080")?
+        .bind(format!("0.0.0.0:{}", *PORT))?
         .workers(4) // turn this into a multi-thread server
         .run()
         .await
